73d91b6a8a9301814ea966dd2fab9cfa253277ca
Kedar Mulay <kedarr@Kedarrs-air.local>
2024-02-22 09:37:18 +0530

    router change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // react
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 // styles
 import './App.css'
